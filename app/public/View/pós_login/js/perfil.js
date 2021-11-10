b60c37e4cdcc956eda67eb95cc7d4ea4982f1d66
GabrielJudeu <68306917+GabrielJudeu@users.noreply.github.com>
2021-11-10 00:13:52 -0300

    funcionamento do botão de editar

--- app/public/View/pós_login/js/perfil.js
+++ app/public/View/pós_login/js/perfil.js
@@ -1,10 +1,11 @@
 function abreModal(modal) {
   if (modal == "add") {
     $("#md-pr").removeClass("md-ct-pos").addClass("md-ct");
+    $("#editar-excluir").removeClass("md-ct").addClass("md-ct-pos");
 
-    $("#formAnuncio").submit(function (e) { 
+    $("#formAnuncio").submit(function (e) {
       e.preventDefault();
-      
+
       var form_data = new FormData();
       form_data.append('titulo', $('input#titulo').val());
       form_data.append('descricao', $('input#descricao').val());
@@ -17,52 +18,56 @@ function abreModal(modal) {
       $("input.botao").prop("disabled", true);
       // processar
       $.ajax({
-          type: "POST",
-          enctype: 'multipart/form-data',
-          url: "/GitHub/Kamaleao/app/private/Controller/controllerCriaServiço.php",
-          data: form_data,
-          processData: false, // impedir que o jQuery tranforma a "data" em querystring
-          contentType: false, // desabilitar o cabeçalho "Content-Type"
-          cache: false, // desabilitar o "cache"
-          // manipular o sucesso da requisição
-          success: function (resposta) {
-            if (resposta == 1) {
-              $("p.form-message").removeClass("sucesso");
-              $("p.form-message").removeClass("erro");
-              $("p.form-message").removeAttr("hidden");
-              $("p.form-message").addClass("sucesso");
+        type: "POST",
+        enctype: 'multipart/form-data',
+        url: "/GitHub/Kamaleao/app/private/Controller/controllerCriaServiço.php",
+        data: form_data,
+        processData: false, // impedir que o jQuery tranforma a "data" em querystring
+        contentType: false, // desabilitar o cabeçalho "Content-Type"
+        cache: false, // desabilitar o "cache"
+        // manipular o sucesso da requisição
+        success: function (resposta) {
+          if (resposta == 1) {
+            $("p.form-message").removeClass("sucesso");
+            $("p.form-message").removeClass("erro");
+            $("p.form-message").removeAttr("hidden");
+            $("p.form-message").addClass("sucesso");
+            $("p.form-message").empty();
+            var sucesso = "<i class='fas fa-exclamation-triangle'></i> <span> Seu novo anúncio foi adicionado!</span>";
+            $(sucesso).appendTo("p.form-message");
+            setTimeout(function () {
+              fechaModal();
+            }, 1000);
+
+          } else {
+            $("p.form-message").removeClass("sucesso");
+            $("p.form-message").removeClass("erro");
+            $("p.form-message").removeAttr("hidden");
+            $("p.form-message").addClass("erro");
+            var erro = "<i class='fas fa-exclamation-triangle'></i> <span> " + resposta + "</span>";
+            var mes = $("p.form-message").html();
+            if (mes.includes("atualizado") == false) {
               $("p.form-message").empty();
-              var sucesso = "<i class='fas fa-exclamation-triangle'></i> <span> Seu novo anúncio foi adicionado!</span>";
-              $(sucesso).appendTo("p.form-message");
-              setTimeout(function () {
-                fechaModal();
-              }, 1000);
-              
-            } else {
-              $("p.form-message").removeClass("sucesso");
-              $("p.form-message").removeClass("erro");
-              $("p.form-message").removeAttr("hidden");
-              $("p.form-message").addClass("erro");
-              var erro = "<i class='fas fa-exclamation-triangle'></i> <span> " + resposta + "</span>";
-              var mes = $("p.form-message").html();
-              if (mes.includes("atualizado") == false) {
-                $("p.form-message").empty();
-                $(erro).appendTo("p.form-message");       
-              }
+              $(erro).appendTo("p.form-message");
             }
-            // reativar o botão de "submit"
-            $("input.botao").prop("disabled", false);
-          },
-          // manipular erros da requisição
-          error: function (e) {
-            alert(e);
-            // reativar o botão de "submit"
-            $("input.botao").prop("disabled", false);
           }
+          // reativar o botão de "submit"
+          $("input.botao").prop("disabled", false);
+        },
+        // manipular erros da requisição
+        error: function (e) {
+          alert(e);
+          // reativar o botão de "submit"
+          $("input.botao").prop("disabled", false);
+        }
       });
 
     });
-    
+
+  }
+  if (modal == "editar-excluir") {
+    $("#md-pr").removeClass("md-ct-pos").addClass("md-ct");
+    $("#formAnuncio").removeClass("form_anuncio").addClass("md-ct-pos");
   }
 }
 
